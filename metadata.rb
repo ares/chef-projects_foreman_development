6e84143576de30e8251344a3faacac71298be6db
Marek Hulan <mhulan@redhat.com>
2016-06-10 13:18:56 +0200

    Add images definition to seed script

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ar3s.cz@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures foreman and related projects from git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.17'
+version          '0.1.18'
 
 depends 'projects', '>= 0.4.7'
 depends 'projects_tmunix', '>= 0.1.1'
