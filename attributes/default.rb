5f33bb4299477ac25efea83e11f1db79645c766b
Marek Hulan <mhulan@redhat.com>
2016-06-14 10:38:10 +0200

    Install openscap plugins

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,7 @@ default[:projects][:foreman] = {
                      project_attributes('foreman_remote_execution'),
                      project_attributes('foreman-tasks'),
                      project_attributes('foreman_templates'),
+                     project_attributes('foreman_openscap'),
                      project_attributes('foreman_chef', :maintain_upstream => true),
                    ]]
 }
@@ -15,13 +16,15 @@ default[:projects][:foreman] = {
 default[:projects][:hammer] = {
   :foreman_url => "https://#{node[:fqdn]}",
   :plugins => Hash[[
-                     project_attributes('hammer-cli-foreman', :module_name => 'hammer_cli_foreman', :gem_name => 'hammer_cli_foreman')
+                     project_attributes('hammer-cli-foreman', :module_name => 'hammer_cli_foreman', :gem_name => 'hammer_cli_foreman'),
+                     project_attributes('hammer_cli_foreman_openscap', :module_name => 'hammer_cli_foreman_openscap', :gem_name => 'hammer_cli_foreman_openscap')
                    ]]
 }
 
 default[:projects][:smart_proxy] = {
   :plugins => Hash[[
                      project_attributes('smart_proxy_dynflow', :module_name => 'dynflow'),
+                     project_attributes('smart_proxy_openscap', :module_name => 'openscap'),
                      project_attributes('smart_proxy_remote_execution_ssh', :module_name => 'remote_execution_ssh'),
                      project_attributes('smart_proxy_chef', :maintain_upstream => true, :module_name => 'chef'),
                    ]]
